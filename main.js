845d10da98e6f6b8d0a7eee42cc732b6661a2b5d
arnugroho <nugroho.akbar.r@gmail.com>
2022-06-15 18:37:50 +0700

    add list incomplete book

---
+++ main.js
@@ -0,0 +1,86 @@
+const books = [];
+const RENDER_EVENT = 'render-book';
+
+document.addEventListener('DOMContentLoaded', function () {
+    const submitForm = document.getElementById('inputBook');
+    submitForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+        addBook();
+    });
+});
+
+
+function addBook() {
+    const inputBookTitle = document.getElementById('inputBookTitle').value;
+    const inputBookAuthor = document.getElementById('inputBookAuthor').value;
+    const inputBookYear = document.getElementById('inputBookYear').value;
+    const inputBookIsComplete = document.getElementById('inputBookIsComplete').checked;
+
+    const generatedID = generateId();
+    const bookObject = generateBookObject(generatedID, inputBookTitle, inputBookAuthor, inputBookYear, inputBookIsComplete);
+    books.push(bookObject);
+
+    document.dispatchEvent(new Event(RENDER_EVENT));
+}
+
+function generateId() {
+    return +new Date();
+}
+
+
+function generateBookObject(id, title, author, year, isComplete) {
+    return {
+        id,
+        title,
+        author,
+        year,
+        isComplete
+    }
+}
+
+document.addEventListener(RENDER_EVENT, function () {
+    console.log(books);
+    const uncompletedTODOList = document.getElementById('incompleteBookshelfList');
+    uncompletedTODOList.innerHTML = '';
+
+    for (const bookItem of books) {
+        if(bookItem.isComplete){
+
+        } else {
+            const bookElement = incompleteBook(bookItem);
+            uncompletedTODOList.append(bookElement);
+        }
+
+    }
+});
+
+function incompleteBook(bookObject) {
+    const textTitle = document.createElement('h3');
+    textTitle.innerText = bookObject.title;
+
+    const textPenulis = document.createElement('p');
+    textPenulis.innerText = 'Penulis: ' + bookObject.author;
+
+    const textTahun = document.createElement('p');
+    textTahun.innerText = 'Tahun: ' + bookObject.year;
+
+    const btnSelesai = document.createElement('button');
+    btnSelesai.innerHTML = 'Selesai dibaca'
+    btnSelesai.classList.add('green');
+
+    const btnHapus = document.createElement('button');
+    btnHapus.innerHTML = 'Hapus buku'
+    btnHapus.classList.add('read');
+
+    const divAction = document.createElement('div');
+    divAction.classList.add('action');
+    divAction.append(btnSelesai, btnHapus)
+
+    const articleBook = document.createElement('article');
+    articleBook.classList.add('book_item')
+    articleBook.append(textTitle, textPenulis, textTahun, divAction)
+
+    articleBook.setAttribute('id', `book-${bookObject.id}`);
+
+    return articleBook;
+}
