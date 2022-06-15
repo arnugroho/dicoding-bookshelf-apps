39b9ab4f5c2480c1fb28e6102941b7ea68f73ed6
arnugroho <nugroho.akbar.r@gmail.com>
2022-06-15 18:40:28 +0700

    add list uncomplete book

--- main.js
+++ main.js
@@ -41,11 +41,14 @@ function generateBookObject(id, title, author, year, isComplete) {
 document.addEventListener(RENDER_EVENT, function () {
     console.log(books);
     const uncompletedTODOList = document.getElementById('incompleteBookshelfList');
+    const completedTODOList = document.getElementById('completeBookshelfList');
     uncompletedTODOList.innerHTML = '';
+    completedTODOList.innerHTML = '';
 
     for (const bookItem of books) {
         if(bookItem.isComplete){
-
+            const bookElement = completeBook(bookItem);
+            completedTODOList.append(bookElement);
         } else {
             const bookElement = incompleteBook(bookItem);
             uncompletedTODOList.append(bookElement);
@@ -66,6 +69,37 @@ function incompleteBook(bookObject) {
 
     const btnSelesai = document.createElement('button');
     btnSelesai.innerHTML = 'Selesai dibaca'
+    btnSelesai.classList.add('green');
+
+    const btnHapus = document.createElement('button');
+    btnHapus.innerHTML = 'Hapus buku'
+    btnHapus.classList.add('read');
+
+    const divAction = document.createElement('div');
+    divAction.classList.add('action');
+    divAction.append(btnSelesai, btnHapus)
+
+    const articleBook = document.createElement('article');
+    articleBook.classList.add('book_item')
+    articleBook.append(textTitle, textPenulis, textTahun, divAction)
+
+    articleBook.setAttribute('id', `book-${bookObject.id}`);
+
+    return articleBook;
+}
+
+function completeBook(bookObject) {
+    const textTitle = document.createElement('h3');
+    textTitle.innerText = bookObject.title;
+
+    const textPenulis = document.createElement('p');
+    textPenulis.innerText = 'Penulis: ' + bookObject.author;
+
+    const textTahun = document.createElement('p');
+    textTahun.innerText = 'Tahun: ' + bookObject.year;
+
+    const btnSelesai = document.createElement('button');
+    btnSelesai.innerHTML = 'Belum selesai di Baca'
     btnSelesai.classList.add('green');
 
     const btnHapus = document.createElement('button');
